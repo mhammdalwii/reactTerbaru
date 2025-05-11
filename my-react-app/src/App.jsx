02836ff73eb36fec7448c3a19333467ecc58710a
Alwiii <210209501011@student.unm.ac.id>
2025-05-11 14:30:09 +0800

    first commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,22 @@
+const Button = (props) => {
+  const { children, variant = "bg-black" } = props;
+  return (
+    <button className={`${variant} text-white font-bold py-2 px-4 rounded`} type="submit">
+      {children}
+    </button>
+  );
+};
+
 function App() {
-  return <div className="flex justify-center">Hello World</div>;
+  return (
+    <div className="flex justify-center bg-blue-400 min-h-screen items-center">
+      <div className="flex gap-x-3">
+        <Button variant="bg-red-700">Login</Button>
+        <Button variant="bg-slate-700">Logout</Button>
+        <Button></Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
