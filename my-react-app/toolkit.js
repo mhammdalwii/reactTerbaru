f86ff76f929988dfb6fe1b308a21cbb262bf4456
Alwiii <210209501011@student.unm.ac.id>
2025-06-13 13:45:10 +0800

    first commit

---
+++ my-react-app/toolkit.js
@@ -0,0 +1,34 @@
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+
+const addToCart = createAction("ADD_TO_CART");
+
+const login = createAction("SESSION_LOGIN");
+
+const loginReducer = createReducer({ status: false }, (builder) => {
+  builder.addCase(login, (state, action) => {
+    state.status = true;
+  });
+});
+
+const cartReducer = createReducer([], (builder) => {
+  builder.addCase(addToCart, (state, action) => {
+    state.push(action.payload);
+  });
+});
+
+const store = configureStore({
+  reducer: {
+    login: loginReducer,
+    cart: cartReducer,
+  },
+});
+
+console.log("oncreate store", store.getState());
+
+store.subscribe(() => {
+  console.log("onchange store", store.getState());
+});
+
+store.dispatch(addToCart({ id: 1, qty: 20 }));
+store.dispatch(addToCart({ id: 2, qty: 10 }));
+store.dispatch(login());
